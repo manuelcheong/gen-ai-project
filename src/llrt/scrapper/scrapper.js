04c96c29a62e2e0fa827a102a08047bd21674098
Manuel Cheong <mcheong@soaint.com>
2025-02-09 18:07:57 +0100

    upload scrp

--- src/llrt/scrapper/scrapper.js
+++ src/llrt/scrapper/scrapper.js
@@ -4,11 +4,49 @@
  * @param {Object} event - The event object.
  * @returns {boolean} - Returns true.
  */
-export const handler = async (event) => {
-  console.log('------ PIPE READER LLRT 😎 CANARY DEPLOYMENT 🐙 AND LLRT WITH SDK 🐀 -----------');
-  console.log(JSON.stringify(event));
 
-  return true;
+const axios = require('axios');
+const cheerio = require('cheerio');
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
+
+const client = new S3Client({ REGION: process.env.REGION });
+
+const bucketName = 'gen-ai-content-pre';
+
+
+const scrapeAndUpload = async (url, index) =>{
+  try {
+      const response = await axios.get(url);
+      const $ = cheerio.load(response.data);
+      const scrapedData = $('body').text(); // Example: Extracting text from <body> tag
+
+      const s3Key = `scraped-data-${index}.txt`;
+      const params = {
+          Bucket: bucketName,
+          Key: s3Key,
+          Body: scrapedData,
+          ContentType: 'text/plain'
+      };
+
+      const command = new PutObjectCommand(params);
+      await client.send(command);
+
+      console.log(`Successfully uploaded data from ${url} to ${s3Key}`);
+  } catch (error) {
+      console.error(`Error scraping ${url}:`, error);
+  }
+}
+
+
+export const handler = async (event) => {
+  console.log('------ WEBSCRAPPING LLRT 😎 CANARY DEPLOYMENT 🐙 AND LLRT WITH SDK 🐀 -----------');
+  
+  const urls = event.urls || [];
+  const promises = urls.map((url, index) => scrapeAndUpload(url, index));
+  await Promise.all(promises);
+  return { message: 'Scraping complete and uploaded to S3' };
+
+  // return true;
 };
 
 
