959658eab0f6b12f4698b1280765999279fa5600
Manuel Cheong <mcheong@soaint.com>
2025-03-09 08:48:28 +0100

    add ttl json

--- src/rs/enrich_events.rs
+++ src/rs/enrich_events.rs
@@ -27,7 +27,10 @@ async fn func(_event: LambdaEvent<Value>) -> Result<Value, Error> {
     
     // Add the TTL field to the event
     if let Value::Object(ref mut map) = payload {
-        map.insert("ttl".to_string(), json!(ttl));
+        // map.insert("ttl".to_string(), json!(ttl));
+        map.insert("additional_data".to_string(), json!({
+            "ttl": ttl
+        }));
     }
     
     print!("Updated Event: {:?}", payload);
