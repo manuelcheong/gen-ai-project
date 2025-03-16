56e267ab56a2c3e5554dbad2f1ce90e03ecb9bbb
Manuel Cheong <mcheong@soaint.com>
2025-03-16 07:17:54 +0100

    updates

--- src/promptui/config.js
+++ src/promptui/config.js
@@ -1,3 +1,4 @@
 window.config = {
-    websocketUrl: 'wss://gvy669mge1.execute-api.eu-west-1.amazonaws.com/pre/' // Replace with your custom WebSocket URL
+    websocketUrl: 'wss://d2pbuhhjlhp1v4.cloudfront.net/pre/'
+    //websocketUrl: 'wss://gvy669mge1.execute-api.eu-west-1.amazonaws.com/pre/' // Replace with your custom WebSocket URL
 };
